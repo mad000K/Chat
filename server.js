5a75094ecd716bce06fddafc49b79ccf5b5c4ac9
525533VbHjY <kravchenkomyronivka@gmail.com>
2025-05-04 17:37:26 +0300

    2

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const db = require('./database');
 
 const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
 const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
 const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+const regFile = fs.readFileSync(path.join(__dirname, 'static', 'register.html'));
 
 const server = http.createServer((req, res) => {
     switch(req.url) {
         case '/': return res.end(indexHtmlFile);
         case '/script.js': return res.end(scriptFile);
         case '/style.css': return res.end(styleFile);
+        case '/register': return res.end(regFile);
     }
     return res.end('Error 404');
 });
@@ -22,16 +23,15 @@ server.listen(3000);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-io.on('connection', async (socket) => {
+io.on('connection', (socket) => {
   console.log('a user connected. id - ' + socket.id);
+  let userNickname = 'user';
 
-  let userNickname = 'admin';
-  let messages = await db.getMessages();
-
-  socket.emit('all_messages', messages);
+  socket.on('set_nickname', (nickname) => {
+    userNickname = nickname;
+  });
 
   socket.on('new_message', (message) => {
-    db.addMessage(message, 1);
     io.emit('message', userNickname + ' : ' + message);
   });
 });
