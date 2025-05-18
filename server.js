fe3e9f8b38d76ee4efe2afae6e05340955ec7a0e
525533VbHjY <kravchenkomyronivka@gmail.com>
2025-05-18 16:18:08 +0300

    3

--- server.js
+++ server.js
@@ -1,39 +1,70 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const db = require('./database');
 
 const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
 const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
-const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
-const regFile = fs.readFileSync(path.join(__dirname, 'static', 'register.html'));
 const authFile = fs.readFileSync(path.join(__dirname, 'static', 'auth.js'));
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+const registerFile = fs.readFileSync(path.join(__dirname, 'static', 'register.html'));
 
 const server = http.createServer((req, res) => {
+  if(req.method === 'GET') {
     switch(req.url) {
-        case '/': return res.end(indexHtmlFile);
-        case '/script.js': return res.end(scriptFile);
-        case '/style.css': return res.end(styleFile);
-        case '/register': return res.end(regFile);
-        case '/auth': return res.end(authFile);
+      case '/': return res.end(indexHtmlFile);
+      case '/script.js': return res.end(scriptFile);
+      case '/auth.js': return res.end(authFile);
+      case '/style.css': return res.end(styleFile);
+      case '/register': return res.end(registerFile);
     }
-    return res.end('Error 404');
+  }
+  if(req.method === 'POST') {
+    switch(req.url) {
+      case '/api/register': return registerUser(req, res);
+    }
+  }
+  return res.end('Error 404');
 });
 
+function registerUser(req, res) {
+    let data = '';
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', async function() {
+      try {
+        const user = JSON.parse(data);
+        if(!user.login || !user.password) {
+          return res.end('Empty login or password');
+        }
+        if(await db.isUserExist(user.login)) {
+          return res.end('User already exist');
+        }
+        await db.addUser(user);
+        return res.end('Registeration is successfull');
+      }
+      catch(e) {
+        return res.end('Error: ' + e);
+      }
+    });
+}
 
 server.listen(3000);
 
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('a user connected. id - ' + socket.id);
-  let userNickname = 'user';
 
-  socket.on('set_nickname', (nickname) => {
-    userNickname = nickname;
-  });
+  let userNickname = 'admin';
+  let messages = await db.getMessages();
+
+  socket.emit('all_messages', messages);
 
   socket.on('new_message', (message) => {
+    db.addMessage(message, 1);
     io.emit('message', userNickname + ' : ' + message);
   });
 });
--- static/script.js
+++ static/script.js
@@ -3,6 +3,15 @@ const messages = document.getElementById('messages');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 
+socket.on('all_messages', function(msgArray) {
+  msgArray.forEach(msg => {
+    let item = document.createElement('li');
+    item.textContent = msg.login + ': ' + msg.content;
+    messages.appendChild(item);
+  });
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   if (input.value) {
@@ -12,17 +21,8 @@ form.addEventListener('submit', function(e) {
 });
 
 socket.on('message', function(msg) {
-    var item = document.createElement('li');
+    let item = document.createElement('li');
     item.textContent = msg;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
-
-function changeNickname() {
-  let nickname = prompt('Choose your nickname');
-  if (nickname) {
-    socket.emit('set_nickname', nickname);
-  }
-}
-
-changeNickname();
